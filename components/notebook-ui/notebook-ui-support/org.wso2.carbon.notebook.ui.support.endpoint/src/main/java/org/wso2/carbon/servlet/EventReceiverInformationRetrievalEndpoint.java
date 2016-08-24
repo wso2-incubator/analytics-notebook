f134807792d41f8ab9f52f340b196f5390818355
nadundesilva <nadunrds@gmail.com>
2016-08-24 16:34:28 +0530

    Added the retrieve event receivers api method

--- components/notebook-ui/notebook-ui-support/org.wso2.carbon.notebook.ui.support.endpoint/src/main/java/org/wso2/carbon/servlet/EventReceiverInformationRetrievalEndpoint.java
+++ components/notebook-ui/notebook-ui-support/org.wso2.carbon.notebook.ui.support.endpoint/src/main/java/org/wso2/carbon/servlet/EventReceiverInformationRetrievalEndpoint.java
@@ -1,17 +1,26 @@
 package org.wso2.carbon.servlet;
 
+import com.google.gson.Gson;
+import org.wso2.carbon.context.PrivilegedCarbonContext;
+import org.wso2.carbon.event.receiver.core.EventReceiverService;
+import org.wso2.carbon.event.receiver.core.config.EventReceiverConfiguration;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 /*
  * Getting HTTP Response for Event Stream Information
  */
 @Path("/event-receiver")
 public class EventReceiverInformationRetrievalEndpoint {
-    public EventReceiverInformationRetrievalEndpoint() {
+    private EventReceiverService eventReceiverService;
 
+    public EventReceiverInformationRetrievalEndpoint() {
+        eventReceiverService = (EventReceiverService) PrivilegedCarbonContext.getThreadLocalCarbonContext()
+                .getOSGiService(EventReceiverService.class, null);
     }
 
     /**
@@ -20,8 +29,16 @@ public class EventReceiverInformationRetrievalEndpoint {
      * @return respnse
      */
     @GET
-    public Response retrieveDatabaseNames() {
-        String jsonString = "Test - Event Receiver";
+    public Response retrieveEventReceiverNames() {
+        List<EventReceiverConfiguration> eventReceiverConfigurations =
+                eventReceiverService.getAllActiveEventReceiverConfigurations();
+        int eventReceiverConfigurationsCount = eventReceiverConfigurations.size();
+        String[] eventReceiverNames = new String[eventReceiverConfigurationsCount];
+        for (int i = 0; i < eventReceiverConfigurationsCount; i++) {
+            eventReceiverNames[i] = eventReceiverConfigurations.get(i).getEventReceiverName();
+        }
+        String jsonString = new Gson().toJson(eventReceiverNames);
+
         return Response.ok(jsonString, MediaType.APPLICATION_JSON).header("Access-Control-Allow-Origin",
                 "*").build();
     }
