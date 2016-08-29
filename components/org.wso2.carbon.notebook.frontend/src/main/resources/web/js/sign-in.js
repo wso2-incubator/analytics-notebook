b94fa2ddb4cc34fa84da8000d3203e801d45ba12
nadundesilva <nadunrds@gmail.com>
2016-08-29 09:11:02 +0530

    Added the tree view notes list to the index and added hyperlinks to the note page

--- components/org.wso2.carbon.notebook.frontend/src/main/resources/web/js/sign-in.js
+++ components/org.wso2.carbon.notebook.frontend/src/main/resources/web/js/sign-in.js
@@ -2,8 +2,16 @@ $(document).ready(function() {
     $("#sign-in").click(function() {
         var credentials = {
             username : $("#username").val(),
-            password : $("#password").val(),
-            keepSignedIn : $("#keep-signed-in").val()
+            password : $("#password").val()
+        };
+        var setError = function(title, message) {
+            $("#error-msg").html("<div id='login-error' class='alert alert-danger' role='alert'>" +
+                "<i class='icon fw fw-error'></i>" +
+                    "<strong>" + title + "</strong> " + message +
+                    "<button type='button' class='close' aria-label='close' data-dismiss='alert'>" +
+                        "<span aria-hidden='true'><i class='fw fw-cancel'></i></span>" +
+                    "</button>" +
+                "</div>");
         };
         if (credentials.username.length > 0 && credentials.password.length > 0) {
             $.ajax({
@@ -14,6 +22,8 @@ $(document).ready(function() {
                     console.log(JSON.stringify(data));
                     if (data.status == constants.response.SUCCESS) {
                         window.location.href = "./";
+                    } else {
+                        setError("Error !", "Invalid Credentials");
                     }
                 }
             });
