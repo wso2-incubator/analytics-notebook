1b3bdd3ccb35bbed719f8ac155806d591c8e17fb
nadundesilva <nadunrds@gmail.com>
2016-09-22 17:10:15 +0530

    Add the delete note functionality and add the get notes list server side functionality

--- app/js/index.js
+++ app/js/index.js
@@ -8,12 +8,26 @@ function Notebook() {
 
     // Private variables
     var utils = new Utils();
-    var notesList = $("#notes-list");
 
     /**
      * Initialize the home page
      */
     self.initialize = function () {
+        loadNoteNamesList();
+
+        // Registering event listeners
+        $("#sign-out").click(function() {
+            utils.signOut("./");
+        });
+
+        $("#create-note").click(function() {
+            showCreateNoteModal();
+        });
+    };
+
+    function loadNoteNamesList() {
+        var notesList = $("#notes-list");
+        utils.showLoadingOverlay(notesList);
         $.ajax({
             type: "GET",
             url: constants.API_URI + "notes",
@@ -26,30 +40,37 @@ function Notebook() {
                     var data = [];
                     $.each(notes, function(index, note) {
                         var row = [];
-                        row.push(note);
+                        row.push("<span class='note-name'>" + note + "</span>");
                         row.push(
                             "<a href='note.html?note=" + note + "' class='btn padding-reduce-on-grid-view'>" +
-                                "<span class='fw-stack'>" +
-                                    "<i class='fw fw-ring fw-stack-2x'></i>" +
-                                    "<i class='fw fw-edit fw-stack-1x'></i>" +
-                                "</span>" +
-                                "<span class='hidden-xs'>Edit</span>" +
+                            "<span class='fw-stack'>" +
+                            "<i class='fw fw-ring fw-stack-2x'></i>" +
+                            "<i class='fw fw-edit fw-stack-1x'></i>" +
+                            "</span>" +
+                            "<span class='hidden-xs'>Edit</span>" +
                             "</a>" +
-                            "<a href='#' class='btn padding-reduce-on-grid-view'>" +
-                                "<span class='fw-stack'>" +
-                                    "<i class='fw fw-ring fw-stack-2x'></i>" +
-                                    "<i class='fw fw-delete fw-stack-1x'></i>" +
-                                "</span>" +
-                                "<span class='hidden-xs'>Delete</span>" +
+                            "<a class='delete-note' class='btn padding-reduce-on-grid-view'>" +
+                            "<span class='fw-stack'>" +
+                            "<i class='fw fw-ring fw-stack-2x'></i>" +
+                            "<i class='fw fw-delete fw-stack-1x'></i>" +
+                            "</span>" +
+                            "<span class='hidden-xs'>Delete</span>" +
                             "</a>"
                         );
                         data.push(row);
                     });
 
-                    notesList.html(utils.generateListTable(columns, data,
+                    var listTable = utils.generateListTable(columns, data,
                         { ordering : false, searching : false },
                         { "Actions" : "text-right" }
-                    ));
+                    );
+
+                    // Registering event listeners to the list table
+                    listTable.find(".delete-note").click(function(event) {
+                        showDeleteNoteModal($(event.target).closest("tr").find(".note-name").html());
+                    });
+
+                    notesList.html(listTable);
                 } else if (response.status == constants.response.NOT_LOGGED_IN) {
                     window.location.href = "sign-in.html";
                 } else {
@@ -61,45 +82,40 @@ function Notebook() {
                 utils.hideLoadingOverlay(notesList);
             }
         });
+    }
 
-        // Registering event listeners
-        $("#sign-out").click(function() {
-            utils.signOut("./");
+    function showCreateNoteModal() {
+        utils.clearPageNotification();
+
+        // Creating the modal content elements
+        var modalBody = $("<div class='pull-left'>");
+        var noteNameInput = $(
+            "<div class='form-group col-sm-12 output-table-container'>" +
+                "<input type='text' class='form-control' />" +
+            "</div>"
+        );
+        var modalFooter = $("<div class='pull-right'>");
+        var createButton = $("<button type='button' class='btn btn-primary'>Create</button>");
+
+        // Appending to create the modal content structure
+        modalBody.append(noteNameInput);
+        modalFooter.append(createButton);
+
+        var modal = utils.showModalPopup("Enter a name for your new note", modalBody, modalFooter);
+
+        // Registering event listeners for the modal window
+        createButton.click(function() {
+            createNote(noteNameInput.children().first().val());
+            modal.modal("hide");
         });
-
-        $("#create-note").click(function() {
-            utils.clearPageNotification();
-
-            // Creating the modal content elements
-            var modalBody = $("<div class='pull-left'>");
-            var noteNameInput = $(
-                "<div class='form-group col-sm-12 output-table-container'>" +
-                    "<input type='text' class='form-control' />" +
-                "</div>"
-            );
-            var modalFooter = $("<div class='pull-right'>");
-            var createButton = $("<button type='button' class='btn btn-primary'>Create</button>");
-
-            // Appending to create the modal content structure
-            modalBody.append(noteNameInput);
-            modalFooter.append(createButton);
-
-            var modal = utils.showModalPopup("Enter a name for your new note", modalBody, modalFooter);
-
-            // Registering event listeners for the modal window
-            createButton.click(function() {
-                createNote(noteNameInput.children().first().val());
-                modal.modal("hide");
-            });
-        });
-    };
+    }
 
     function createNote(name) {
         if (name.indexOf(' ') > 0) {
             utils.handlePageNotification("error", "Error", "Note name cannot contain white spaces");
         } else {
             $.ajax({
-                type: "PUT",
+                type: "POST",
                 url: constants.API_URI + "notes/" + name,
                 success: function (response) {
                     if (response.status == constants.response.SUCCESS) {
@@ -115,5 +131,44 @@ function Notebook() {
                 }
             });
         }
+    }
+
+    function showDeleteNoteModal(noteName) {
+        utils.clearPageNotification();
+
+        // Creating the modal content elements
+        var modalFooter = $("<div class='pull-right'>");
+        var deleteButton = $("<button type='button' class='btn btn-primary'>Delete</button>");
+
+        // Appending to create the modal content structure
+        modalFooter.append(deleteButton);
+
+        var modal = utils.showModalPopup("Do you want to delete " + noteName + " ?", $(), modalFooter);
+
+        // Registering event listeners for the modal window
+        deleteButton.click(function() {
+            deleteNote(noteName);
+            modal.modal("hide");
+        });
+    }
+
+    function deleteNote(name) {
+        $.ajax({
+            type: "DELETE",
+            url: constants.API_URI + "notes/" + name,
+            success: function (response) {
+                if (response.status == constants.response.SUCCESS) {
+                    utils.handlePageNotification("info", "Info", "Note successfully deleted");
+                    loadNoteNamesList();
+                } else if (response.status == constants.response.NOT_LOGGED_IN) {
+                    window.location.href = "sign-in.html";
+                } else {
+                    utils.handlePageNotification("error", "Error", response.message);
+                }
+            },
+            error : function(response) {
+                utils.handlePageNotification("error", "Error", utils.generateErrorMessageFromStatusCode(response.readyState));
+            }
+        });
     }
 }
--- components/org.wso2.carbon.notebook.core/src/main/java/org/wso2/carbon/notebook/core/util/NoteUtils.java
+++ components/org.wso2.carbon.notebook.core/src/main/java/org/wso2/carbon/notebook/core/util/NoteUtils.java
@@ -1,6 +1,5 @@
 package org.wso2.carbon.notebook.core.util;
 
-import org.wso2.carbon.analytics.spark.core.exception.AnalyticsPersistenceException;
 import org.wso2.carbon.notebook.commons.constants.NoteConstants;
 import org.wso2.carbon.notebook.core.ServiceHolder;
 import org.wso2.carbon.notebook.core.exception.NotebookPersistenceException;
@@ -15,6 +14,25 @@ import org.wso2.carbon.registry.core.utils.RegistryUtils;
  * General utility functions for the notebook
  */
 public class NoteUtils {
+    public static String[] getAllNotes(int tenantID) throws RegistryException {
+        UserRegistry userRegistry = ServiceHolder.getRegistryService().getConfigSystemRegistry(tenantID);
+        createNotesCollectionIfNotExists(userRegistry);
+        Collection noteCollection = (Collection) userRegistry.get(NoteConstants.NOTE_LOCATION);
+        String[] notes = noteCollection.getChildren();
+
+        if (notes == null) {
+            notes = new String[0];
+        } else {
+            for (int i = 0; i < notes.length; i++) {
+                // Removing note registry path
+                notes[i] = notes[i].replace(NoteConstants.NOTE_LOCATION, "");
+                notes[i] = notes[i].replace(RegistryConstants.PATH_SEPARATOR, "");
+                notes[i] = notes[i].replace(NoteConstants.NOTE_FILE_EXTENSION_SEPARATOR + NoteConstants.NOTE_FILE_EXTENSION, "");
+            }
+        }
+        return notes;
+    }
+
     public static void addNewNote(int tenantID, String noteName) throws NotebookPersistenceException, RegistryException {
         UserRegistry userRegistry = ServiceHolder.getRegistryService().getConfigSystemRegistry(tenantID);
         createNotesCollectionIfNotExists(userRegistry);
@@ -57,8 +75,13 @@ public class NoteUtils {
         }
     }
 
-    public static void deleteNote(int tenantID, String noteName) {
+    public static void deleteNote(int tenantID, String noteName) throws RegistryException {
+        UserRegistry userRegistry = ServiceHolder.getRegistryService().getConfigSystemRegistry(tenantID);
+        String noteLocation = getScriptLocation(noteName);
 
+        if (userRegistry.resourceExists(noteLocation)) {
+            userRegistry.delete(noteLocation);
+        }
     }
 
     private static void createNotesCollectionIfNotExists(UserRegistry registry) throws RegistryException {
